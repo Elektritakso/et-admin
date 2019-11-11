f7b78ce8b399225c7ea0b4d8961b072fd96a57a8
Elektritakso <pavel.timerhanov@khk.ee>
2019-11-11 14:44:27 +0200

    MongoDB collections needed for system

---
+++ collections.js
@@ -0,0 +1,25 @@
+// MongoDB collections we need to use for this system
+// Translation collection
+Translations = new Mongo.Collection("translations");
+// Config collection
+CommonConfig = new Mongo.Collection("commonconfig");
+// Test collection for dev purpose
+Test = new Mongo.Collection("test");
+// Errors collection
+Errors = new Mongo.Collection("erros");
+// Cars collection()
+Cars = new Mongo.Collection("cars");
+// Shift collection
+Shifts = new Mongo.Collection("shifts");
+// Orders collections
+Orders = new Mongo.Collection("orders");
+// Routes for orders collection
+OrderRoutes = new Mongo.Collection("orderroute");
+// client collection
+Clients = new Mongo.Collection("clients");
+// Client address collection for caching
+ClientAddresses = new Mongo.Collection("clientaddresses");
+
+
+console.log("Rendering collections");
+
